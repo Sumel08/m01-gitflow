0837f6dca673fb4a5ed361536ca16de55decb7cd
Oscar Lemus <oscar@lemus.app>
2023-09-13 19:03:47 -0600

    feat: add subtraction operation

--- js/main.js
+++ js/main.js
@@ -8,3 +8,16 @@ document.getElementById('addition-form').addEventListener('submit', event => {
   const addition = addend1 + addend2;
   document.getElementById('addition-result').innerText = `The addition of ${addend1} plus ${addend2} is ${addition}`;
 });
+
+document.getElementById('subtraction-form').addEventListener('submit', event => {
+  event.preventDefault();
+
+  const form = event.target;
+  const formFields = form.elements;
+  const minuend = Number(formFields.minuend.value);
+  const subtrahend = Number(formFields.subtrahend.value);
+  const subtraction = minuend - subtrahend;
+  document.getElementById(
+    'subtraction-result',
+  ).innerText = `The subtraction of ${minuend} minus ${subtrahend} is ${subtraction}`;
+});
